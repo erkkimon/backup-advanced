f7c4bc186f0e98788854890d71bb9c7e0fc53f12
Tom <Tom@Airi-Petteri.local>
2014-02-17 00:16:59 +0200

    Just committed the first version, it should work fine.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ backup-advanced
 ===============
 
 This script is a universal backup script for sites using content management system (CMS) such as MediaWiki, Joomla and WordPress meant to be run daily using crontab. This script creates a copy of both database and files.
+
+The basic idea of the script is to store daily versions of the site for a week backwards. Although, the first backup of each month will be saved permanently. This means that you should create "cyclic" and "monthly" directories in the directory meant to contain the backups.
+
+The script is very simple and meant to be a base or sceleton when creating a backup script. Anyway, it also works as is. Just fill in the correct backup information in these quite self-explanatory variables. This script is published in public domain so feel free to modify it to fit your needs! The base is written by Tom Himanen (tom.himanen [at] gmail.com).
---
+++ backup-advanced.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# MySQL login information
+mysql_username=""
+mysql_user_password=""
+mysql_db_name=""
+
+# Site address will be included in the file name of the backup, so
+# basically it can be anything. This is just an identifier.
+
+#site_address="example.com"
+site_address=""
+
+# Absolute path to the directory in which the backups will be stored.
+# The path is defined without the trailing slash. See the example below: 
+
+#dir_for_backups="/home/user/backups"
+dir_for_backups=""
+
+# The absolute location of the files of the site to be backed up without
+# the trailing slash. See the example below:
+
+#dir_to_be_backed_up="/home/user"
+dir_to_be_backed_up=""
+
+# Create the backup directories if needed
+if [ ! -d ${dir_for_backups}         ]; then mkdir ${dir_for_backups};         fi
+if [ ! -d ${dir_for_backups}/monthly ]; then mkdir ${dir_for_backups}/monthly; fi
+if [ ! -d ${dir_for_backups}/cyclic  ]; then mkdir ${dir_for_backups}/cyclic;  fi
+
+mysqldump       -u${mysql_username} -p${mysql_user_password} \
+                ${mysql_db_name} | gzip > \
+                ${dir_for_backups}/${site_address}_db_$(date +%y%m%d).sql.gz
+
+tar cjf         ${dir_for_backups}/${site_address}_files_$(date +%y%m%d).tbz \
+                --exclude ${dir_for_backups} \
+                ${dir_to_be_backed_up}
+
+if [ $(date +%d) = 01 ]; then
+                cp ${dir_for_backups}/${site_address}_db_$(date +%y%m%d)*.tbz \
+                ${dir_for_backups}/monthly/
+fi 
+
+mv              ${dir_for_backups}/${site_address}_files_$(date +%y%m%d).tbz \
+                ${dir_for_backups}/cyclic/${site_address}_files_$(date +%a).tbz
+
+mv              ${dir_for_backups}/${site_address}_db_$(date +%y%m%d).sql.gz \
+                ${dir_for_backups}/cyclic/${site_address}_db_$(date +%a).sql.gz
+
+# TODO
+
+# idea by icelesstea
+# BEFORE: if [ ! -d ${dir_for_backups} ]; then mkdir ${dir_for_backups}; fi
+# AFTER:  [ ! -d ${dir_for_backups} ] && mkdir ${dir_for_backups}
+
+# idea by icelesstea
+# BEFORE: /bin/bash
+# AFTER:  /usr/bin/env
